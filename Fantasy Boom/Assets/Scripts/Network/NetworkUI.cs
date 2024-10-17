2494f8bbe0945c50498005fe7e6e37e1dfe31141
NotEnoughMax <73028659+Sccara@users.noreply.github.com>
2024-10-17 15:01:27 +0300

    Added basic multiplayer

---
+++ Fantasy Boom/Assets/Scripts/Network/NetworkUI.cs
@@ -0,0 +1,21 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkUI : MonoBehaviour
+{
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+
+    public void StartServer()
+    {
+        NetworkManager.Singleton.StartServer();
+    }
+}
